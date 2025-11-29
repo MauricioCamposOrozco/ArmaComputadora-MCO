00fed43616b8475046b7e187f0353219b2ece074
MauricioCamposOrozco <al45090@un.edu.mx>
2025-11-28 23:22:16 -0600

    Avance 10

--- calculo.js
+++ calculo.js
@@ -1,6 +1,9 @@
 var formulario = document.querySelector("form");
-var resultado = document.getElementById("resultado");
 
+var resultado = document.createElement("div");
+resultado.id = "resultado";
+document.body.appendChild(resultado);
+    
 formulario.addEventListener("submit", function(e) {
     e.preventDefault();
 
@@ -8,19 +11,21 @@ formulario.addEventListener("submit", function(e) {
 
     var gabinete = document.getElementById("gabinete").value;
     var procesador = document.getElementById("procesador").value;
-    var discoduro = document.getElementById("discoduro").value;
+    var disco = document.getElementById("discoduro").value;
     var memoria = document.getElementById("memoria").value;
+    var monitor = document.getElementById("monitor").value;
 
     total = total + Number(gabinete);
     total = total + Number(procesador);
     total = total + Number(disco);
     total = total + Number(memoria);
+    total = total + Number(monitor);
 
-    var accesoriosSeleccionados = document.querySelectorAll(".accesorios input:checked")
+    var accesoriosSeleccionados = document.querySelectorAll(".accesorios input:checked");
     var cantidad = accesoriosSeleccionados.length;
 
     total = total + (cantidad * 100);
 
     resultado.textContent = "Total: $" + total;
 
-    });
+});
